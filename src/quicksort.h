958be795402dc8fd88d6d9b1ef83ec8b51639432
AshwinRG1 <91225319+AshwinRG1@users.noreply.github.com>
2026-03-20 17:43:47 +0000

    fixes to quicksort

--- src/quicksort.h
+++ src/quicksort.h
@@ -1,16 +1,22 @@
 #pragma once
 
 #include <vector>
+#include <algorithm>
+#include "flight.h"
 
 using namespace std;
 
-void quickSort(vector<Flight>& data, int& swaps){
+void quickSort(vector<Flight> data,int low, int high, int& swaps){
 
-    
+    if (data.at(low).total_delay() < data.at(high).total_delay()){
+        int pivot = partition(data, low, high, swaps);
+        quickSort(data,low, pivot-1, swaps);
+        quickSort(data, pivot+1, high, swaps);
+    }
 
 }
 
-int partition (vector<Flight>& data, int& low, int& high, int& swaps){
+int partition (vector<Flight> data, int low, int high, int& swaps){
 
     int pivot = data.at(low).total_delay();
     int up = low;
@@ -19,16 +25,16 @@ int partition (vector<Flight>& data, int& low, int& high, int& swaps){
     while (up < down){
         for (int i = up; i < high; i++){
             if (data.at(i).total_delay() > pivot){
-                up++;
                 break;
             }
+            up++;
         }
 
         for (int i = down; i > low; i--){
             if(data.at(i).total_delay() < pivot){
-                down--;
                 break;
             }
+            down--;
         }
 
         if (up < down){
