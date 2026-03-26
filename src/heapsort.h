32956b1badc966939257caad7a2ff311270d120d
AshwinRG1 <91225319+AshwinRG1@users.noreply.github.com>
2026-03-26 20:10:18 +0000

    Update heapsort.h

--- src/heapsort.h
+++ src/heapsort.h
@@ -3,51 +3,43 @@
 
 #include "flight.h"
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 //Pseudocode from Discussion 6 & 7 Slides
 
-void heapify(vector<Flight>& data, int size, int parent, int& swaps){
+void heapSort(vector<Flight>& data, int swaps, int size){
+
+
+    int index = size/2 - 1;
+
+    for (int i = index; i>=0; i--){
+        heapify(data, size, i);
+    }
+
+
+}
+
+void heapify(vector<Flight>& data, int size, int parent){
 
     int left = 2*parent + 1;
     int right = 2*parent + 2;
 
     int max = parent;
 
-    if (left < size && data.at(left).total_delay() > data.at(max).total_delay()){
-        max = left;
-    }
-
     if (right < size && data.at(right).total_delay() > data.at(max).total_delay()){
         max = right;
+    }
+    if (left < size && data.at(left).total_delay() > data.at(max).total_delay()){
+        max = left;
     }
 
     if (max != parent){
         swap(data.at(parent), data.at(max));
         swaps++;
-        heapify(data, size, max, swaps);
+        heapify(data, size, max);
     }
 
 }
-
-void heapSort(vector<Flight>& data, int& swaps, int size){
-
-
-    int index = size/2 - 1;
-
-    for (int i = index; i>=0; i--){
-        heapify(data, size, i, swaps);
-    }
-
-    for (int i = size-1; i > 0; i--){
-        swap(data.at(0), data.at(i));
-        swaps++;
-        heapify(data, i, 0, swaps);
-    }
-
-}
-
 
 
