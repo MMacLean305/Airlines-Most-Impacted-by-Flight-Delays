d428f53364e7733e771f64024d1bb9b13b0a09b5
ashwin <gudaar80@gmail.com>
2026-03-27 10:57:32 -0400

    added comments to quicksort discussing design changes

--- src/quicksort.h
+++ src/quicksort.h
@@ -7,9 +7,10 @@
 using namespace std;
 
 //Pseudocode from Discussion 8 Slides
+//https://www.geeksforgeeks.org/dsa/quick-sort-algorithm/
 
 int partition (vector<Flight>& data, int low, int high, int& swaps){
-
+    //changed pivot to middle to make it more efficient and fix runtime error/out of bounds error
      int mid = (low + high) / 2;
      swap(data.at(low), data.at(mid));
      swaps++;
@@ -41,7 +42,7 @@ int partition (vector<Flight>& data, int low, int high, int& swaps){
 }
 
 void quickSort(vector<Flight>& data,int low, int high, int& swaps){
-
+    //additional clauses were added due to runtime errors, worked on small datasets lagged out and stopped working on the 100k+ dataset.
      if (low < high){
          int pivot = partition(data, low, high, swaps);
          if (pivot > low) {
