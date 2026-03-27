59eaecf5f67e44e68cd4476455171c1a46343c38
ashwin <gudaar80@gmail.com>
2026-03-27 10:39:41 -0400

    quick sort finally works i used to pray for times like this

--- src/quicksort.h
+++ src/quicksort.h
@@ -10,46 +10,46 @@ using namespace std;
 
 int partition (vector<Flight>& data, int low, int high, int& swaps){
 
-    // int mid = (low + high) / 2;
-    // swap(data.at(low), data.at(mid));
-    // swaps++;
-    //
-    // int pivot = data.at(low).total_delay();
-    // int up = low + 1;
-    // int down = high;
-    //
-    //
-    // while (true){
-    //     while (up<=high && data.at(up).total_delay() < pivot) {
-    //         up++;
-    //     }
-    //
-    //     while (down >= low && data.at(down).total_delay() >= pivot) {
-    //         down--;
-    //     }
-    //
-    //     if (up >= down) {
-    //         break;
-    //     }
-    //     swap(data.at(up), data.at(down));
-    //     swaps++;
-    //
-    // }
-    // swap(data.at(low), data.at(down));
-    // swaps++;
-    // return down;
+     int mid = (low + high) / 2;
+     swap(data.at(low), data.at(mid));
+     swaps++;
+
+     int pivot = data.at(low).total_delay();
+     int up = low + 1;
+     int down = high;
+
+
+     while (true){
+         while (up<=high && data.at(up).total_delay() < pivot) {
+             up++;
+         }
+
+         while (down > low && data.at(down).total_delay() >= pivot) {
+             down--;
+         }
+
+         if (up >= down) {
+             break;
+         }
+         swap(data.at(up), data.at(down));
+         swaps++;
+
+     }
+     swap(data.at(low), data.at(down));
+     swaps++;
+     return down;
 }
 
 void quickSort(vector<Flight>& data,int low, int high, int& swaps){
 
-    // if (low < high){
-    //     int pivot = partition(data, low, high, swaps);
-    //     if (pivot > low) {
-    //         quickSort(data,low, pivot -1, swaps);
-    //     }
-    //     if (pivot < high) {
-    //         quickSort(data, pivot + 1, high, swaps);
-    //     }
-    // }
+     if (low < high){
+         int pivot = partition(data, low, high, swaps);
+         if (pivot > low) {
+             quickSort(data,low, pivot -1, swaps);
+         }
+         if (pivot < high) {
+             quickSort(data, pivot + 1, high, swaps);
+         }
+     }
 
 }
